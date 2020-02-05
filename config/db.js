4272c64f6d4b4aeae1204257f812d4e1f1a89555
Mohamed lachref Aberkane <maberkan@z4r4p1.le-101.fr>
2020-02-05 09:07:30 +0100

    change db

--- config/db.js
+++ config/db.js
@@ -2,30 +2,18 @@ var mysql = require('mysql')
 var faker = require('faker')
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : 'remotemysql.com',
   port     : 3306,
-  user     : 'user',
-  password : 'pass',
+  user     : 'Wn9Gou8rYs',
+  password : 'AuvqSBo9YN',
+  database : 'Wn9Gou8rYs'
 })
-
-connection.query("CREATE DATABASE IF NOT EXISTS matcha", function(err, res) {
-  if (err) throw err;
-  console.log('Created !');
-});
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "user",
-  port: 3306,
-  password: "pass",
-  database: "matcha"
-});
 
 connection.connect(function(err) {
   if (err) return console.error('error: ' + err.message);
 });
 
-connection.query('USE matcha')
+connection.query('USE Wn9Gou8rYs')
 connection.query('CREATE TABLE IF NOT EXISTS users (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, username VARCHAR(100) NOT NULL, lastname VARCHAR(100) NOT NULL, firstname VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, inscription_date DATE, reset VARCHAR(255) DEFAULT \'NULL\', sexe VARCHAR(25), orientation VARCHAR(25) DEFAULT \'Bisexuelle\', bio VARCHAR(10000), interests VARCHAR(255), age INT, pic0 VARCHAR(255), pic1 VARCHAR(255), pic2 VARCHAR(255), pic3 VARCHAR(255), pic4 VARCHAR(255), city VARCHAR(255), lat FLOAT, lon FLOAT, visit DATE, online BOOLEAN DEFAULT FALSE, confirme BOOLEAN DEFAULT FALSE, confirmeKey VARCHAR(255) DEFAULT \'NULL\')')
 connection.query('CREATE TABLE IF NOT EXISTS likes (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, username VARCHAR(100) NOT NULL, liked VARCHAR(100) NOT NULL)')
 connection.query('CREATE TABLE IF NOT EXISTS visits (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, username VARCHAR(100) NOT NULL, visited VARCHAR(100) NOT NULL)')
